afa9cc76abf06aa40437ea3e40d3a5b978d7ef6b
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-01 19:11:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const config = require("./config/config");
 const db = require("./config/mongoose");
+const router = require("./routes");
 //create an instance of express
 const app = express();
 const PORT = config.PORT || process.env.PORT || 4040;
-app.use("/", (req, res) => {
-    return res.send("Hello Server");
-});
+app.use(express.json());
+app.use(router);
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
 });
