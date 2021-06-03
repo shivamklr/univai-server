4a866bad996b6e80e75a68bcd045adad7c318257
Shivam Kalra <shivamkalra98@gmail.com>
2021-06-03 19:42:21 +0530

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const cors = require("cors");
 const config = require("./config/config");
 const db = require("./config/mongoose");
 const router = require("./routes");
 //create an instance of express
 const app = express();
 const PORT = config.PORT || process.env.PORT || 4040;
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.listen(PORT, () => {
